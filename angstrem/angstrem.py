2bdde2423773208520300d98ea249794ccbf9c5d
Strider <strange.transistor@gmail.com>
2019-01-26 22:19:10 +0300

    parse example.

--- angstrem/angstrem.py
+++ angstrem/angstrem.py
@@ -1,4 +1,16 @@
 
+
+from .rootpath import rootpath_from as rootpath
+
+root = rootpath(__file__)
+root = rootpath(root())
+
+
 from .grammar.grammar import grammar
 
-print(grammar())
+
+example = open(root('example/example')).read()
+
+
+ast = grammar().parse(example)
+print(ast)
