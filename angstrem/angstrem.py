008d5c5d6a8c4ce17ab435d795f59c95d31482a0
Strider <strange.transistor@gmail.com>
2019-01-27 01:37:16 +0300

    visitor.

--- angstrem/angstrem.py
+++ angstrem/angstrem.py
@@ -7,10 +7,32 @@ root = rootpath(root())
 
 
 from .grammar.grammar import grammar
+angstrem = grammar()
 
 
 example = open(root('example/example')).read()
 
 
-ast = grammar().parse(example)
-print(ast)
+ast = angstrem.parse(example)
+print(1, ast)
+
+
+from parsimonious.nodes import NodeVisitor
+
+visitor = NodeVisitor()
+visitor.grammar = angstrem
+
+def noop (*_): pass
+
+def v (node, children):
+	print(node.expr.name, node.text)
+	print(node.match)
+	print('---')
+	# return '000',
+
+visitor.generic_visit = noop
+visitor.visit_word = v
+
+visitor.visit(ast)
+
+print(2, ast)
