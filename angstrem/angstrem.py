1665bda6a022c26e2ca4d6399a4107a33d705e8f
Strider <strange.transistor@gmail.com>
2019-04-13 19:28:20 +0300

    recursive grammar.

--- angstrem/angstrem.py
+++ angstrem/angstrem.py
@@ -15,6 +15,12 @@ example = open(root('example/example')).read()
 
 ast = angstrem.parse(example)
 
-print(ast.data)
-print(ast.children)
-print(ast.meta)
+# print(ast)
+
+# print(ast.data)
+# print(ast.children)
+# print(ast.meta)
+
+for t in ast.iter_subtrees():
+# for t in ast.iter_subtrees_topdown():
+	print(t)
