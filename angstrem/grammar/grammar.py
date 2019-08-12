c40fbcc440f932989a70657ff5351c36167261ad
Strider <strange.transistor@gmail.com>
2019-08-12 19:42:51 +0300

    extract grammar.

--- angstrem/grammar/grammar.py
+++ angstrem/grammar/grammar.py
@@ -1,42 +1,18 @@
 
-
-#from ..rootpath import rootpath_from as rootpath
-
-#root = rootpath(__file__)
-
-#def read (*path):
-#	full = root(*path) + '.lark'
-
-#	return open(full).read()
-
-
-#from lark import Lark
-
-#def grammar ():
-#	return Lark(read('grammar'), start = 'main')
-
-
 from textx import metamodel_from_str
 
 def grammar ():
-	return metamodel_from_str("""
-	Model: stmt *= Stmt;
-	Stmt: Decl | Expr;
-	Decl: Decl__Func;
+	return metamodel_from_str(read('grammar'))
 
-	Decl__Func: 'fn' ID '(' Func__Args? ')' Func__Body;
-	Func__Args: ID ( ',' ID )*;
-	Func__Body: Expr;
 
-	Expr:
-		Block | Comment;
+#
+def read (*path):
+	full = root(*path) + '.textx'
 
-	Block: '{' Block__Body? '}';
-	Block__Body: Expr ( Delimiter Expr )* ;
+	return open(full).read()
 
-	Delimiter: ';' | '\n';
 
-	Comment: Comment__Line | Comment__Block;
-	Comment__Line: /\/\/.*$/;
-	Comment__Block: /\/\*.*?\*\//;
-	""")
+#
+from ..rootpath import rootpath_from as rootpath
+
+root = rootpath(__file__)
