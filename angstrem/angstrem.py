2bd08d4c39127a5d98b53e2cbc5d9f1d6afced96
Strider <strange.transistor@gmail.com>
2019-08-12 20:34:06 +0300

    visitor.

--- angstrem/angstrem.py
+++ angstrem/angstrem.py
@@ -6,6 +6,7 @@ root = rootpath(__file__)
 root = rootpath(root())
 
 
+#
 from .grammar.grammar import grammar
 angstrem = grammar()
 
@@ -13,5 +14,24 @@ example = open(root('example/example')).read()
 ast = angstrem.model_from_str(example)
 
 
-for x in ast.stmt:
-	print(x)
+#
+class Visitor:
+	def __init__ (self, node_type):
+		self.node_type = node_type
+
+	def visit (self, ast):
+		for node in getattr(ast, self.node_type):
+			type_name = node.__class__.__name__
+			getattr(self, type_name)(node)
+
+class AngstremVisitor (Visitor):
+	def __init__ (self):
+		super().__init__('stmt')
+
+	def Decl__Func (self, node):
+		print(node.name, node.args)
+
+
+#
+v = AngstremVisitor()
+v.visit(ast)
